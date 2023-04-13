c7e3ae3ef6c3942080445db1495135d0f2db7339
hhhhh <52508643+gggvik@users.noreply.github.com>
2023-04-13 16:32:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 chat_id = 250483508 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     imp_crit = 0.04
-    t, p_value = ttest_ind(x, y)
+    t, p_value = mannwhitneyu(x, y)
     return (p_value < imp_crit)
