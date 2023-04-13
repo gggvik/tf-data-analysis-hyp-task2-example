014b14c17d07de6af4d0c8753789cdbca487ad83
hhhhh <52508643+gggvik@users.noreply.github.com>
2023-04-13 16:14:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 250483508 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    mp_crit = 0.04
+    t, p_value = ttest_ind(x, y)
+    return (p_value < imp_crit)
